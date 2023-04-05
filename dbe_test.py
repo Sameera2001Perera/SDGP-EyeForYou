7decb5f4a3bb651f905625593853e786e7515cb8
2021121212 <108779115+Sameera2001Perera@users.noreply.github.com>
2023-04-06 01:22:23 +0530

    try unit tests

--- dbe_test.py
+++ dbe_test.py
@@ -5,3 +5,28 @@ def euclaidean_distance(point, point1):
     x2, y2 = point1
     distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
     return distance
+
+
+def focal_length(refDistance, rf_imageWidth):
+    """
+    The purpose of this function is calculating the focal length. The distance between the camera lens and the CMOS sensor
+    is called as focal length. Distance from object to the Camera while Capturing Reference image, Actual width of object
+    and width of object in reference image. When the user moves closer to the screen and moves away from the screen,
+    Actual width of object (face width) does not change. Therefore, this function does not consider Actual width of object.
+    :param refDistance: Distance from object to the Camera while Capturing Reference image
+    :param rf_imageWidth: width of object in reference image
+    :return: It returns the focal length of camera
+    """
+    focal_length = (rf_imageWidth * refDistance)
+    return focal_length
+
+
+def distance_measure(focalLength, frame_face_width):
+    """
+    The purpose of this function is measuring the distance between face and the camera.
+    :param focalLength: Returned value of focal_length(refDistance, rf_imageWidth) function.
+    :param frame_face_width: Width of the frame.(A video is considered as a set of frames)
+    :return: It returns the distance between face and the camera.
+    """
+    distance = focalLength / frame_face_width
+    return distance
--- test_units.py
+++ test_units.py
@@ -3,3 +3,16 @@ import pytest
 
 def test_euclaidean_distance():
     assert dbe_test.euclaidean_distance((2,5),(4,8)) == 3.605551275463989
+
+
+def test_focal_length():
+    assert dbe_test.focal_length(6,5) == 30
+    assert dbe_test.focal_length(23,5)==115
+
+
+def test_distance_measure():
+    assert dbe_test.distance_measure(8,2) == 4
+    assert dbe_test.distance_measure(9, 2) == 4.5
+
+
+
